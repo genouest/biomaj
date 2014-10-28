1b15bdbbd6a258fb8cd6eb23eb93991606657961
Olivier Sallou <olivier.sallou@irisa.fr>
2014-10-28 08:58:38 +0100

    start command line options

--- bin/biomaj-cli.py
+++ bin/biomaj-cli.py
@@ -9,10 +9,10 @@ from biomaj.config import BiomajConfig
 def main():
 
   parser = OptionParser()
-
   parser.add_option('-c', '--config', dest="config",help="Configuration file")
-  parser.add_option('-u', '--update', dest="bank",help="Update a bank")
+  parser.add_option('-u', '--update', dest="update", help="Update action", action="store_true", default=False)
   parser.add_option('-s', '--status', dest="status", help="Get status", action="store_true", default=False)
+  parser.add_option('-b', '--bank', dest="bank",help="bank name")
 
   (options, args) = parser.parse_args()
 
@@ -20,10 +20,33 @@ def main():
   if options.config is not None:
     BiomajConfig.load_config(options.config)
 
+  if options.status:
+    if options.bank:
+      bank = Bank(options.bank)
+    else:
+      print '#' * 80
+      print "# Name\tType\tRelease"
+      banks = Bank.list()
+      for bank in banks:
+        '''
+        production = { 'release': self.session.get('release'),
+                        'session': self.session._session['id'],
+                        'data_dir': self.config.get('data.dir'),
+                        'prod_dir': self.session.get_release_directory()}
+        '''
+        if 'current' in bank and bank['current']:
+          for prod in production:
+            if bank['current'] == prod['session']:
+              release = prod['release']
+        else:
+          release = None
+        print " "+bank['name']+"\t"+bank['type']+"\t"+str(release)
+      print '#' * 80
+      return
 
-  if options.bank:
-    bmaj = Bank(options.bank)
-    bmaj.update()
+  if options.update and options.bank:
+    bmaj = Bank(options.bank, options)
+    return bmaj.update()
 
 if __name__ == '__main__':
     main()

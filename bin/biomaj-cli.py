fa9c629effaea52db42c5aab464651ea61e7d647
Olivier Sallou <olivier.sallou@irisa.fr>
2015-05-11 10:12:57 +0200

    fix shebang to get Python from env

--- bin/biomaj-cli.py
+++ bin/biomaj-cli.py
@@ -1,8 +1,8 @@
+#!/usr/bin/env python
 from __future__ import print_function
 from future import standard_library
 standard_library.install_aliases()
 from builtins import str
-#!/usr/bin/python
 
 import os,sys
 #from optparse import OptionParser

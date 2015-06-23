8e4b9ddb7f0e03baf1ce7d2abd54c820f3445c40
Olivier Sallou <olivier.sallou@irisa.fr>
2015-06-23 09:10:15 +0200

    increase version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ config = {
     'url': 'http://biomaj.genouest.org',
     'download_url': 'http://biomaj.genouest.org',
     'author_email': 'olivier.sallou@irisa.fr',
-    'version': '3.0.7',
+    'version': '3.0.8',
      'classifiers': [
         # How mature is this project? Common values are
         #   3 - Alpha

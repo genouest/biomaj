193b615f47ee682f61987be6e0bfb6320f953afc
chloe riou <chloe.riou@inria.fr>
2016-09-26 16:05:48 +0200

    test on travis-ci : correction 3.4 and 3.5

--- biomaj/download/rsync.py
+++ biomaj/download/rsync.py
@@ -174,12 +174,12 @@ class RSYNCDownload(DownloadInterface):
         return(error)
 #---------------------------------------------------------------
     def test_stderr_rsync_error(self, stderr):
-        if "rsync error" in stderr:
+        if "rsync error" in str(stderr):
             reason = stderr.split(str(self.protocol) + " error:")[1].split("\n")[0]
             raise ExceptionRsync(reason)
 #---------------------------------------------------------------
     def test_stderr_rsync_message(self, stderr):
-        if "rsync:" in stderr:
+        if "rsync:" in str(stderr):
             reason = stderr.split(str(self.protocol) + ":")[1].split("\n")[0]
             raise ExceptionRsync(reason)
 

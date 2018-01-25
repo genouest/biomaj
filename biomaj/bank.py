1896ab29b4266ce411e7b1fe9bc4a4a046b59ae9
mboudet <mateo.boudet@irisa.fr>
2018-01-25 20:49:28 +0100

    Add format in get /bank/{bank} (#93)
    * API should return formats

--- biomaj/bank.py
+++ biomaj/bank.py
@@ -164,7 +164,7 @@ class Bank(object):
                               str(last_update),
                               str(release)])
             # Bank production info header
-            prod_info.append(["Session", "Remote release", "Release", "Directory", "Freeze"])
+            prod_info.append(["Session", "Remote release", "Release", "Directory", "Freeze", "Format(s)"])
             for prod in _bank['production']:
                 data_dir = self.config.get('data.dir')
                 dir_version = self.config.get('dir.version')
@@ -176,16 +176,21 @@ class Bank(object):
                                            dir_version,
                                            prod['prod_dir'])
                 date = datetime.fromtimestamp(prod['session']).strftime('%Y-%m-%d %H:%M:%S')
+                formats = ""
+                # Check the value exist , is not empty, and a list.
+                if 'formats' in prod and prod['formats'] and isinstance(prod['formats'], list):
+                    formats = str(','.join(prod['formats']))
                 prod_info.append([date,
                                   prod['remoterelease'],
                                   prod['release'],
                                   release_dir,
-                                  'yes' if 'freeze' in prod and prod['freeze'] else 'no'])
+                                  'yes' if 'freeze' in prod and prod['freeze'] else 'no',
+                                  formats])
             # Bank pending info header
             if 'pending' in _bank and len(_bank['pending']) > 0:
                 pend_info.append(["Pending release", "Last run"])
                 for pending in _bank['pending']:
-                    run=""
+                    run = ""
                     try:
                         run = datetime.fromtimestamp(pending['id']).strftime('%Y-%m-%d %H:%M:%S')
                     except Exception as e:

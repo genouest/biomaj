014aa09eb3bb6a9fe07d9b5b7900d7fba12a7c2f
root <root@biomaj.novalocal>
2018-01-25 13:36:29 +0000

    API should return formats

--- biomaj/bank.py
+++ biomaj/bank.py
@@ -164,7 +164,7 @@ class Bank(object):
                               str(last_update),
                               str(release)])
             # Bank production info header
-            prod_info.append(["Session", "Remote release", "Release", "Directory", "Freeze"])
+            prod_info.append(["Session", "Remote release", "Release", "Directory", "Freeze", "Format(s)"])
             for prod in _bank['production']:
                 data_dir = self.config.get('data.dir')
                 dir_version = self.config.get('dir.version')
@@ -180,7 +180,8 @@ class Bank(object):
                                   prod['remoterelease'],
                                   prod['release'],
                                   release_dir,
-                                  'yes' if 'freeze' in prod and prod['freeze'] else 'no'])
+                                  'yes' if 'freeze' in prod and prod['freeze'] else 'no'],
+                                  str(','.join(prod['formats'])))
             # Bank pending info header
             if 'pending' in _bank and len(_bank['pending']) > 0:
                 pend_info.append(["Pending release", "Last run"])

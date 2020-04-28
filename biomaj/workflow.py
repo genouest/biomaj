ffcfdf077a8133e60f754a81214a521778e96be8
Olivier Sallou <olivier.sallou@irisa.fr>
2020-04-28 18:33:18 +0200

    fix regression in file save_as

--- biomaj/workflow.py
+++ biomaj/workflow.py
@@ -1310,18 +1310,18 @@ class UpdateWorkflow(Workflow):
             for f in downloader.files_to_download:
                 if 'save_as' not in f or not f['save_as']:
                     f['save_as'] = f['name']
-                    for p in cf.get('remote.files', default='.*').split():
-                        if p == '.*' or p == '**/*':
-                            continue
-                        if p.startswith('^'):
-                            p = p.replace('^', '^/')
-                        else:
-                            p = '/' + p
-                        res = re.match(p, f['name'])
-                        if res is not None and res.groups() is not None and len(res.groups()) >= 1:
-                            f['save_as'] = '/'.join(res.groups())
-                            break
-
+                for p in cf.get('remote.files', default='.*').split():
+                    if p == '.*' or p == '**/*':
+                        continue
+                    if p.startswith('^'):
+                        p = p.replace('^', '^/')
+                    else:
+                        p = '/' + p
+                    res = re.match(p, f['name'])
+                    if res is not None and res.groups() is not None and len(res.groups()) >= 1:
+                        f['save_as'] = '/'.join(res.groups())
+                        break
+        
             files_to_download += downloader.files_to_download
 
         self.session.set('download_files', downloader.files_to_download)
